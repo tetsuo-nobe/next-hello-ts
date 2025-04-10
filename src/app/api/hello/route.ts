cf5b9a728a14ddb936b5c6f97da7a19cc51a57d7
Workshop Participant <participant@workshops.aws>
2025-04-10 04:33:22 +0000

    first version

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,6 @@
+export async function POST(request: Request) {
+  const reqBody: any = await request.json()  
+  const name: string = reqBody.yourname
+  const msg: string = `こんにちは！ ${name}`  
+  return Response.json({ message: msg })
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,40 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import {useState} from "react"
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+const MainPage = () => {  
+  const [name, setName] = useState("")   
+  const [message, setMessage] = useState("")
+  
+  const handleSubmit = async (e) => {    
+    e.preventDefault()
+    try {      
+      const response = await fetch("/api/hello", {        
+        method: "POST",        
+        headers: {          
+          "Accept": "application/json",          
+          "Content-Type": "application/json"        
+        },        
+        body: JSON.stringify({          
+          yourname : name        
+        })       
+      })    
+      const jsonData = await response.json()
+      setMessage(jsonData.message)
+    } catch {          
+      alert("handleSubmit Error")
+    }  
+    
+  }    
+  return (    
+    <div>      
+      <h1>Next.js サンプル</h1>      
+      <form onSubmit={handleSubmit}>        
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} name="yourname" placeholder="お名前を入力" required />        
+        <button>Greeting</button>      
+      </form>    
+      <h3>{message}</h3>
     </div>
-  );
+  )
 }
+
+export default MainPage
